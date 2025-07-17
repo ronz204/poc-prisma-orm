044bdfbaad334688816b359b169b9eccaed4ff2f
roney ruiz <roneyruizrojas@gmail.com>
2025-07-16 20:44:58 -0600

    feat: create plan table migration

---
+++ src/database/schemas/20250716-2023-create-plan-table/up.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE Plans (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  monthly_price NUMERIC(10, 2) NOT NULL,
+  periodicity_days INTEGER NOT NULL,
+  is_active BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
