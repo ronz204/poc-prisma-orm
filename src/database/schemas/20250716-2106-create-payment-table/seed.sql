810c332fdf9b1efaf966e2f047febe941c292c4c
roney ruiz <roneyruizrojas@gmail.com>
2025-07-17 15:38:55 -0600

    feat: create payment seed
    Signed-off-by: roney ruiz <roneyruizrojas@gmail.com>

---
+++ src/database/schemas/20250716-2106-create-payment-table/seed.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO Payments (invoice_id, amount_paid, payment_date, payment_method) VALUES
+(1, 9.99, '2024-01-16 11:00:00', 'credit_card'),
+(2, 9.99, '2024-02-16 10:30:00', 'debit_card'),
+(3, 19.99, '2024-02-02 12:00:00', 'bank_transfer'),
+(4, 19.99, '2024-03-02 11:45:00', 'credit_card'),
+(5, 29.99, '2024-03-11 09:30:00', 'debit_card'),
+(6, 9.99, '2024-04-06 15:00:00', 'credit_card'),
+(7, 19.99, '2024-05-21 17:00:00', 'bank_transfer'),
+(10, 0.00, '2025-07-10 08:05:00', 'cash'),
+(11, 29.99, '2023-11-02 11:00:00', 'credit_card');
--- src/database/schemas/20250716-2106-create-payment-table/up.sql
+++ src/database/schemas/20250716-2106-create-payment-table/up.sql
@@ -4,7 +4,7 @@ CREATE TABLE Payments (
   invoice_id INTEGER NOT NULL REFERENCES Invoices(id),
   amount_paid NUMERIC(10, 2) NOT NULL,
   payment_date TIMESTAMP NOT NULL,
-  payment_method VARCHAR(255) NOT NULL CHECK (payment_method IN ('credit card', 'debit card', 'cash', 'bank transfer')),
+  payment_method VARCHAR(255) NOT NULL CHECK (payment_method IN ('credit_card', 'debit_card', 'cash', 'bank_transfer')),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
