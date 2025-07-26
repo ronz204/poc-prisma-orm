3dbf2e234db7304b811e827a42c42b91f386ff70
roney ruiz <roneyruizrojas@gmail.com>
2025-07-25 21:22:43 -0600

    chore: purge old structure and files

--- src/database/schemas/20250716-2023-create-plan-table/seed.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO Plans (name, description, monthly_price, periodicity_days, is_active) VALUES
-('Basic Plan', 'Access to essential features, ideal for individual users.', 9.99, 30, TRUE),
-('Standard Plan', 'All Basic Plan features plus additional storage and standard support.', 19.99, 30, TRUE),
-('Premium Plan', 'Full access to all features, 24/7 priority support, and advanced functionalities.', 29.99, 30, TRUE),
-('Annual Basic Plan', 'Save 10% with annual payment for the Basic Plan.', 99.99, 365, TRUE),
-('Annual Premium Plan', 'The best option for unlimited access and VIP support for a full year.', 299.99, 365, TRUE),
-('Free Trial', 'Enjoy one week of full access at no cost.', 0.00, 7, FALSE);
