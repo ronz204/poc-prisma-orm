80ab0474a37bca72e56d85517ee1e5517f4067df
roney ruiz <roneyruizrojas@gmail.com>
2025-07-26 19:51:43 -0600

    feat: create users schema and migration

---
+++ prisma/migrations/20250727014916_create_users_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "biography" TEXT,
+    "pictureUrl" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_name_key" ON "Users"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
