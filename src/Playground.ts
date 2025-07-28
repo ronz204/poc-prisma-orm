62d84179ac6852ed7f41481e335a1ae6057c8d85
roney ruiz <roneyruizrojas@gmail.com>
2025-07-27 20:41:29 -0600

    chore: add the use case to playground

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,4 +1,6 @@
 import { Prisma } from "@Database/Prisma";
 import { CreateUserUseCase } from "@UseCases/Users/Create/CreateUserUseCase";
+import { UpdateUserUseCase } from "@UseCases/Users/Update/UpdateUserUseCase";
 
 const createUserUseCase = new CreateUserUseCase(Prisma);
+const updateUserUseCase = new UpdateUserUseCase(Prisma);
