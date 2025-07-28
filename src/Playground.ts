0283f4fe3b4d82e497098ee168e6e61631b9fba6
roney ruiz <roneyruizrojas@gmail.com>
2025-07-27 20:16:32 -0600

    chore: remove data from playground

--- src/Playground.ts
+++ src/Playground.ts
@@ -2,10 +2,3 @@ import { Prisma } from "@Database/Prisma";
 import { CreateUserUseCase } from "@UseCases/Users/Create/CreateUserUseCase";
 
 const createUserUseCase = new CreateUserUseCase(Prisma);
-/* const response = await createUserUseCase.execute({
-  name: "John Doe",
-  email: "john.doe@example.com",
-  password: "securepassword",
-  biography: "Hello, I'm John!",
-  pictureUrl: "http://example.com/john.jpg"
-}); */
