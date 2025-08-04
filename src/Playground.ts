1d5be6ab4f8b06127beccfba62ea461b5cd1139c
roney ruiz <roneyruizrojas@gmail.com>
2025-08-03 22:00:19 -0600

    feat: add get active subs use case to playground

--- src/Playground.ts
+++ src/Playground.ts
@@ -2,7 +2,9 @@ import { Prisma } from "@Database/Prisma";
 import { CreateUserUseCase } from "@UseCases/Users/Create/CreateUserUseCase";
 import { UpdateUserUseCase } from "@UseCases/Users/Update/UpdateUserUseCase";
 import { DisableUserUseCase } from "@UseCases/Users/Disable/DisableUserUseCase";
+import { GetActiveSubsUseCase } from "@UseCases/Users/GetActiveSubs/GetActiveSubsUseCase";
 
 const createUserUseCase = new CreateUserUseCase(Prisma);
 const updateUserUseCase = new UpdateUserUseCase(Prisma);
 const disableUserUseCase = new DisableUserUseCase(Prisma);
+const getActiveSubsUseCase = new GetActiveSubsUseCase(Prisma);
