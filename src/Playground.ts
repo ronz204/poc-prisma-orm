56cb6594a65eb1301eaac3d5effc7f7d117bc2de
roney ruiz <roneyruizrojas@gmail.com>
2025-08-05 12:09:10 -0600

    feat: add new use case to playground

--- src/Playground.ts
+++ src/Playground.ts
@@ -2,11 +2,13 @@ import { Prisma } from "@Database/Prisma";
 import { CreateUserUseCase } from "@UseCases/Users/Create/CreateUserUseCase";
 import { UpdateUserUseCase } from "@UseCases/Users/Update/UpdateUserUseCase";
 import { CreatePlanUseCase } from "@UseCases/Plans/Create/CreatePlanUseCase";
+import { UpdatePlanUseCase } from "@UseCases/Plans/Update/UpdatePlanUseCase";
 import { DisableUserUseCase } from "@UseCases/Users/Disable/DisableUserUseCase";
 import { GetActiveSubsUseCase } from "@UseCases/Users/GetActiveSubs/GetActiveSubsUseCase";
 
 const createUserUseCase = new CreateUserUseCase(Prisma);
 const updateUserUseCase = new UpdateUserUseCase(Prisma);
 const createPlanUseCase = new CreatePlanUseCase(Prisma);
+const updatePlanUseCase = new UpdatePlanUseCase(Prisma);
 const disableUserUseCase = new DisableUserUseCase(Prisma);
 const getActiveSubsUseCase = new GetActiveSubsUseCase(Prisma);
