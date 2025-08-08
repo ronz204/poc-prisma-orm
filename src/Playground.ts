78cbf5d9400a8bcf3f39cb6f62536ea1cfa4b921
roney ruiz <roneyruizrojas@gmail.com>
2025-08-07 20:42:00 -0600

    chore: rename register user use case

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,16 +1,16 @@
 import { Prisma } from "@Database/Prisma";
 import { ListPlansUseCase } from "@UseCases/Plans/List/ListPlansUseCase";
-import { CreateUserUseCase } from "@UseCases/Users/Create/CreateUserUseCase";
 import { UpdateUserUseCase } from "@UseCases/Users/Update/UpdateUserUseCase";
 import { CreatePlanUseCase } from "@UseCases/Plans/Create/CreatePlanUseCase";
 import { UpdatePlanUseCase } from "@UseCases/Plans/Update/UpdatePlanUseCase";
 import { DisableUserUseCase } from "@UseCases/Users/Disable/DisableUserUseCase";
+import { RegisterUserUseCase } from "@UseCases/Users/RegisterUser/RegisterUserUseCase";
 import { GetActiveSubsUseCase } from "@UseCases/Users/GetActiveSubs/GetActiveSubsUseCase";
 
 const listPlansUseCase = new ListPlansUseCase(Prisma);
-const createUserUseCase = new CreateUserUseCase(Prisma);
 const updateUserUseCase = new UpdateUserUseCase(Prisma);
 const createPlanUseCase = new CreatePlanUseCase(Prisma);
 const updatePlanUseCase = new UpdatePlanUseCase(Prisma);
 const disableUserUseCase = new DisableUserUseCase(Prisma);
+const registerUserUseCase = new RegisterUserUseCase(Prisma);
 const getActiveSubsUseCase = new GetActiveSubsUseCase(Prisma);
