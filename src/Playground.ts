cdbdca6d15258ddb22f0672ee423873a2b53367b
roney ruiz <roneyruizrojas@gmail.com>
2025-08-07 20:55:56 -0600

    chore: rename create plan use case

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,7 +1,7 @@
 import { Prisma } from "@Database/Prisma";
 import { ListPlansUseCase } from "@UseCases/Plans/List/ListPlansUseCase";
-import { CreatePlanUseCase } from "@UseCases/Plans/Create/CreatePlanUseCase";
 import { UpdatePlanUseCase } from "@UseCases/Plans/Update/UpdatePlanUseCase";
+import { CreatePlanUseCase } from "@UseCases/Plans/CreatePlan/CreatePlanUseCase";
 import { UpdateUserUseCase } from "@UseCases/Users/UpdateUser/UpdateUserUseCase";
 import { DisableUserUseCase } from "@UseCases/Users/DisableUser/DisableUserUseCase";
 import { RegisterUserUseCase } from "@UseCases/Users/RegisterUser/RegisterUserUseCase";
