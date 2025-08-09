35888c33d2685ee798537efeb0eed35557eb6127
roney ruiz <roneyruizrojas@gmail.com>
2025-08-09 11:42:09 -0600

    feat: create user factory

---
+++ src/Infrastructure/Database/Factories/UserFactory.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+import type { User } from "generated/prisma";
+
+export class UserFactory {
+  public static async build(user: Partial<User>): Promise<User> {
+    return {
+      id: user.id ?? faker.number.int({ min: 1, max: 1000 }),
+      name: user.name ?? faker.person.fullName(),
+      email: user.email ?? faker.internet.email(),
+      password: user.password ?? faker.internet.password(),
+      isActive: user.isActive ?? faker.datatype.boolean(),
+      biography: user.biography ?? faker.lorem.paragraph(),
+      pictureUrl: user.pictureUrl ?? faker.image.url(),
+      createdAt: user.createdAt ?? new Date(),
+      updatedAt: user.updatedAt ?? new Date(),
+    };
+  };
+};
