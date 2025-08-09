ad9928a2dea4b3b175c920e3458fc41899e65927
roney ruiz <roneyruizrojas@gmail.com>
2025-08-08 18:33:58 -0600

    feat: update runner with new seeders

--- src/Infrastructure/Database/Runner.ts
+++ src/Infrastructure/Database/Runner.ts
@@ -2,12 +2,16 @@ import { Prisma } from "./Prisma";
 import { UserSeeder } from "./Seeders/UserSeeder";
 import { PlanSeeder } from "./Seeders/PlanSeeder";
 import { SubsSeeder } from "./Seeders/SubsSeeder";
+import { PaymentSeeder } from "./Seeders/PaymentSeeder";
+import { InvoiceSeeder } from "./Seeders/InvoiceSeeder";
 
 export class Runner {
   public static async execute(): Promise<void> {
     await new UserSeeder(Prisma).seed();
     await new PlanSeeder(Prisma).seed();
     await new SubsSeeder(Prisma).seed();
+    await new InvoiceSeeder(Prisma).seed();
+    await new PaymentSeeder(Prisma).seed();
   };
 };
 
