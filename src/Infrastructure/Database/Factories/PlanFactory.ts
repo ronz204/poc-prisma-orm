da7f3357edf84d097c413167c5a3e5fab2962eec
roney ruiz <roneyruizrojas@gmail.com>
2025-08-09 12:39:31 -0600

    feat: implement plan factory in the seeder

--- src/Infrastructure/Database/Factories/PlanFactory.ts
+++ src/Infrastructure/Database/Factories/PlanFactory.ts
@@ -2,18 +2,23 @@ import { faker } from "@faker-js/faker";
 import { Prisma } from "generated/prisma";
 import type { Plan } from "generated/prisma";
 
-const PLAN_PERIODS = [7, 30, 365] as const;
-const PLAN_NAMES = ["Basic", "Standard", "Premium", "Annual", "Trial"];
+const DEFINITIONS = [
+  { name: "Basic", period: 30, price: 9.99 },
+  { name: "Standard", period: 30, price: 19.99 },
+  { name: "Premium", period: 30, price: 29.99 },
+  { name: "Annual", period: 365, price: 99.99 },
+  { name: "Trial", period: 7, price: 0.0 },
+];
 
 export class PlanFactory {
-  public static async build(plan: Partial<Plan>): Promise<Plan> {
-    const price = plan.price ?? faker.number.float({ min: 10, max: 100 });
+  public static async build(plan: Partial<Plan>) {
+    const definition = DEFINITIONS.find(p => p.name === plan.name)!;
 
     return {
-      id: plan.id ?? faker.number.int({ min: 1, max: 1000 }),
-      name: plan.name ?? faker.helpers.arrayElement(PLAN_NAMES),
-      price: new Prisma.Decimal(price),
-      period: plan.period ?? faker.helpers.arrayElement(PLAN_PERIODS),
+      id: plan.id ?? faker.number.int({ min: 1, max: 100 }),
+      name: definition.name,
+      period: definition.period,
+      price: new Prisma.Decimal(definition.price),
       isActive: plan.isActive ?? faker.datatype.boolean(),
       createdAt: plan.createdAt ?? new Date(),
       updatedAt: plan.updatedAt ?? new Date(),
