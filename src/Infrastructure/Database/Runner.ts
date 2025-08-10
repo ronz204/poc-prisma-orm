fea4552a19804fcdc654673a1544012c70a5ddae
roney ruiz <roneyruizrojas@gmail.com>
2025-08-09 23:02:34 -0600

    chore: disable other seeders and factories

--- src/Infrastructure/Database/Runner.ts
+++ src/Infrastructure/Database/Runner.ts
@@ -2,16 +2,12 @@ import { Prisma } from "./Prisma";
 import { UserSeeder } from "./Seeders/UserSeeder";
 import { PlanSeeder } from "./Seeders/PlanSeeder";
 import { SubsSeeder } from "./Seeders/SubsSeeder";
-import { PaymentSeeder } from "./Seeders/PaymentSeeder";
-import { InvoiceSeeder } from "./Seeders/InvoiceSeeder";
 
 export class Runner {
   public static async execute(): Promise<void> {
     await new UserSeeder(Prisma).seed();
     await new PlanSeeder(Prisma).seed();
     await new SubsSeeder(Prisma).seed();
-    await new InvoiceSeeder(Prisma).seed();
-    await new PaymentSeeder(Prisma).seed();
   };
 };
 
