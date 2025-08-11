5b8ac41aa1d05f08d273465d77e57090f7358df5
roney ruiz <roneyruizrojas@gmail.com>
2025-08-10 20:10:46 -0600

    feat: create between random method in luxon helper

--- src/Infrastructure/Helpers/LuxonHelper.ts
+++ src/Infrastructure/Helpers/LuxonHelper.ts
@@ -4,4 +4,8 @@ export class LuxonHelper {
   public static plus(date: Date, days: number): Date {
     return DateTime.fromJSDate(date).plus({ days }).toJSDate();
   };
+
+  public static between(start: Date, end: Date): Date {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  };
 };
