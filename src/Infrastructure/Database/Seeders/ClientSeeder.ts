fe6476210737b5e669934ab9f6e74bef43c44658
roney ruiz <roneyruizrojas@gmail.com>
2025-08-11 21:27:48 -0600

    refac: structure dara correctly

--- src/Infrastructure/Database/Seeders/ClientSeeder.ts
+++ src/Infrastructure/Database/Seeders/ClientSeeder.ts
@@ -10,7 +10,12 @@ export class ClientSeeder extends Seeder {
   public async seed(): Promise<void> {
     await Promise.all(Array.from({ length: 15 }).map(async () => {
       const client = await ClientFactory.build({});
-      await this.prisma.client.create({ data: client });
+      await this.prisma.client.create({
+        data: {
+          name: client.name,
+          email: client.email,
+        },
+      });
     }));
   };
 };
