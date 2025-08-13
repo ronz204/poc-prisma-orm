9ad12250ad6ad94082b489cbe82a33064d4671ed
roney ruiz <roneyruizrojas@gmail.com>
2025-08-12 21:02:27 -0600

    feat: adjust client dto

--- src/Application/DTOs/ClientDTO.ts
+++ src/Application/DTOs/ClientDTO.ts
@@ -1,5 +1,13 @@
+import { Prisma } from "generated/prisma";
+
 export interface ClientDTO {
   id: number;
   name: string;
   email: string;
+  subs: {
+    plan: string;
+    status: string;
+    period: number;
+    price: Prisma.Decimal;
+  }[];
 };
