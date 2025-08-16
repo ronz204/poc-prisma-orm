d43000c5e7403ef44691f84a950f4434a8cf37e4
roney ruiz <roneyruizrojas@gmail.com>
2025-08-15 21:40:37 -0600

    feat: add new use to playground

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,16 +1,25 @@
 import { Prisma } from "@Database/Prisma";
-import { GetPlansUseCase } from "@UseCases/Plans/GetPlans/GetPlansUseCase";
-import { GetClientUseCase } from "@UseCases/Clients/GetClient/GetClientUseCase";
-import { CreatePlanUseCase } from "@UseCases/Plans/CreatePlan/CreatePlanUseCase";
-import { UpdatePlanUseCase } from "@UseCases/Plans/UpdatePlan/UpdatePlanUseCase";
-import { GetClientsUseCase } from "@UseCases/Clients/GetClients/GetClientsUseCase";
-import { CreateClientUseCase } from "@UseCases/Clients/CreateClient/CreateClientUseCase";
-import { UpdateClientUseCase } from "@UseCases/Clients/UpdateClient/UpdateClientUseCase";
 
+import { SubscribeToUseCase } from "@UseCases/Subs/SubscribeTo/SubscribeToUseCase";
+const subscribeToUseCase = new SubscribeToUseCase(Prisma);
+
+import { GetPlansUseCase } from "@UseCases/Plans/GetPlans/GetPlansUseCase";
 const getPlansUseCase = new GetPlansUseCase(Prisma);
-const getClientUseCase = new GetClientUseCase(Prisma);
-const getClientsUseCase = new GetClientsUseCase(Prisma);
+
+import { CreatePlanUseCase } from "@UseCases/Plans/CreatePlan/CreatePlanUseCase";
 const createPlanUseCase = new CreatePlanUseCase(Prisma);
+
+import { UpdatePlanUseCase } from "@UseCases/Plans/UpdatePlan/UpdatePlanUseCase";
 const updatePlanUseCase = new UpdatePlanUseCase(Prisma);
+
+import { GetClientUseCase } from "@UseCases/Clients/GetClient/GetClientUseCase";
+const getClientUseCase = new GetClientUseCase(Prisma);
+
+import { GetClientsUseCase } from "@UseCases/Clients/GetClients/GetClientsUseCase";
+const getClientsUseCase = new GetClientsUseCase(Prisma);
+
+import { CreateClientUseCase } from "@UseCases/Clients/CreateClient/CreateClientUseCase";
 const createClientUseCase = new CreateClientUseCase(Prisma);
+
+import { UpdateClientUseCase } from "@UseCases/Clients/UpdateClient/UpdateClientUseCase";
 const updateClientUseCase = new UpdateClientUseCase(Prisma);
