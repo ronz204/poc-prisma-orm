72a646d364f08d198dd0086d4a132eda47be312b
roney ruiz <roneyruizrojas@gmail.com>
2025-09-27 21:37:00 -0600

    chore: remove old migrations files

--- prisma/migrations/20250811024049_remove_index_from_subs/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Subscription_userId_planId_key";
--- prisma/migrations/20250818014005_one_subscription_per_client/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[clientId]` on the table `Subscription` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Subscription_clientId_key" ON "Subscription"("clientId");
