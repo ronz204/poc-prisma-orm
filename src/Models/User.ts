780bd3b28207f4f10d20a6227ed7a826b7b36deb
roney ruiz <roneyruizrojas@gmail.com>
2025-10-02 22:27:19 -0600

    feat: create user namespace to encapsulate types

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,6 +1,13 @@
 import { Prisma } from "generated/prisma";
+import type { User as Model } from "generated/prisma";
 
 export namespace User {
+  export type Entity = Model;
+  export type Records = Model[];
+
   export type Include = Prisma.UserInclude;
   export type Where = Prisma.UserWhereInput;
+
+  export type Create = Prisma.UserCreateInput;
+  export type Update = Prisma.UserUpdateInput;
 };
