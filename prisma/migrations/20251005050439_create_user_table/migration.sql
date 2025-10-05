371d43b541e6f5ba176fa0ca07cd35211c3d72e3
roney ruiz <roneyruizrojas@gmail.com>
2025-10-04 23:18:53 -0600

    feat: generate migration for user table

---
+++ prisma/migrations/20251005050439_create_user_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(30) NOT NULL,
+    "country" VARCHAR(100) NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "public"."User"("phone");
