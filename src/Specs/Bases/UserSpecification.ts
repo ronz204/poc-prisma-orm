e8e3fa9064ebc7c1798c9b0e4e938a822a8ab116
roney ruiz <roneyruizrojas@gmail.com>
2025-10-08 23:43:40 -0600

    feat: add unique property to user spec base

--- src/Specs/Bases/UserSpecification.ts
+++ src/Specs/Bases/UserSpecification.ts
@@ -2,10 +2,16 @@ import type { User } from "@Models/User";
 import { Specification } from "./Specification";
 
 export abstract class UserSpecification extends Specification {
-  protected where: User.Where = {};
+  protected where: User.Where = {} as User.Where;
+  protected unique: User.Unique = {} as User.Unique;
+
   public abstract toQuery(): object;
 
   public setWhere(value: User.Where): this {
     this.where = value; return this
+  };
+
+  public setUnique(value: User.Unique): this {
+    this.unique = value; return this
   };
 };
