29f9fb9bf80bd79a75bbc3584499d0682090ce80
roney ruiz <roneyruizrojas@gmail.com>
2025-10-17 19:30:00 -0600

    chore: discard seeders and commands

--- prisma/migrations/20251018011506_create_seeder_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Seeder" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "executedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Seeder_pkey" PRIMARY KEY ("id")
-);
