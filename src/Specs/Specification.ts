47eed12a8fe7cf6f73cc7d39f46fb89d30f49e3a
roney ruiz <roneyruizrojas@gmail.com>
2025-10-18 18:48:00 -0600

    refac: adjust pagination setter in spec base

--- src/Specs/Specification.ts
+++ src/Specs/Specification.ts
@@ -2,11 +2,9 @@ export class Specification {
   protected skip: number = 0;
   protected take: number = 10;
 
-  public setSkip(value: number): this {
-    this.skip = value; return this
-  };
-
-  public setTake(value: number): this {
-    this.take = value; return this
+  public setPagination(page: number, limit: number): this {
+    this.skip = (page - 1) * limit;
+    this.take = limit;
+    return this;
   };
 };
