594544a5154501869fbf33e585682f4a2debdf59
roney ruiz <roneyruizrojas@gmail.com>
2025-10-29 22:16:53 -0600

    refac: make simple retrieve customer fixture

--- tests/Fixtures/RetrieveCustomerFixture.ts
+++ tests/Fixtures/RetrieveCustomerFixture.ts
@@ -26,20 +26,11 @@ export const RetrieveCustomerFixture = {
   service: service,
   contract: contract,
   customer: {
-    id: customer.id,
-    name: customer.name,
-    email: customer.email,
-    phone: customer.phone,
-    active: customer.active,
-    country: customer.country,
-    contracts: [
-      {
-        id: contract.id,
-        service: service,
-        active: contract.active,
-        startDate: contract.startDate,
-      },
-    ],
+    ...customer,
+    contracts: [{
+      ...contract,
+      service: service,
+    }],
   },
   query: {
     valid: {
