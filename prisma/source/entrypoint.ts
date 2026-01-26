5bf00e3c1cb35d906c802c0b5f278d8da8a5add1
roney ruiz <roneyruizrojas@gmail.com>
2026-01-25 21:34:04 -0600

    feat: add ts path for prisma generated

--- prisma/source/entrypoint.ts
+++ prisma/source/entrypoint.ts
@@ -1,1 +1,12 @@
-console.log("Hello from Prisma!!!");
+import type { User } from "@Prisma/client";
+
+const user: User = {
+  id: 1,
+  name: "Jhon Doe",
+  email: "",
+  password: "",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+console.log(user);
