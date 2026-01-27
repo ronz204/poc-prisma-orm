2bc1d064b53739d585d7c2f3febc74720a8fbd23
roney ruiz <roneyruizrojas@gmail.com>
2026-01-26 20:26:24 -0600

    feat: create books model and migration

---
+++ prisma/prisma/migrations/20260127022404_create_book_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "core"."books" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
