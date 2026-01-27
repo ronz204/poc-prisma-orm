0cd29511e82666af9194f35216bd400f65755d53
roney ruiz <roneyruizrojas@gmail.com>
2026-01-26 21:41:47 -0600

    chore: purge library domain modeling

--- prisma/prisma/migrations/20260127022404_create_book_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "core"."books" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
-);
