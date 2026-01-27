1a43f76d38b5f1d29db437e7cd1ef7b156bb6d8a
roney ruiz <roneyruizrojas@gmail.com>
2026-01-26 22:06:32 -0600

    feat: modeling teams and migrate

---
+++ prisma/prisma/migrations/20260127040527_create_teams_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "esports"."teams" (
+    "id" SERIAL NOT NULL,
+    "tag" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "teams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teams_tag_key" ON "esports"."teams"("tag");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teams_name_key" ON "esports"."teams"("name");
