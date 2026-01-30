745f4f7ee794964914a5ada0cf63c6515d7cc02b
roney ruiz <roneyruizrojas@gmail.com>
2026-01-29 20:34:05 -0600

    chore: move connector to infra layer

--- prisma/source/Infrastructure/Database/Connector.ts
+++ prisma/source/Infrastructure/Database/Connector.ts
@@ -4,4 +4,4 @@ import { PrismaClient } from "@Prisma/client";
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-export const Prisma = new PrismaClient({ adapter });
+export const Conn = new PrismaClient({ adapter });
