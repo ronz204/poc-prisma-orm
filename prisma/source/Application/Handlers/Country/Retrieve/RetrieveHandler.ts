a77cbc7579e52b860c8525a7de713055f16eae7e
roney ruiz <roneyruizrojas@gmail.com>
2026-01-30 22:46:17 -0600

    feat: add prisma injection to retrieve countries handler

--- prisma/source/Application/Handlers/Country/Retrieve/RetrieveHandler.ts
+++ prisma/source/Application/Handlers/Country/Retrieve/RetrieveHandler.ts
@@ -2,17 +2,18 @@ import type { Handler } from "@Handlers/Handler";
 import type { RetrieveCountriesQuery as Query } from "./RetrieveQuery";
 import type { RetrieveCountriesResponse as Response } from "./RetrieveResponse";
 
-import { Prisma } from "@Database/Connector";
+import { PrismaClient } from "@Prisma/client";
 import { RetrieveCountriesSpec } from "./RetrieveSpec";
 import { RetrieveCountriesSchema } from "./RetrieveSchema";
 
 export class RetrieveCountriesHandler implements Handler<Query, Response> {
+  constructor(private prisma: PrismaClient) {};
 
   public async handle(input: Query): Promise<Response> {
     const validated = RetrieveCountriesSchema.parse(input);
 
-    const spec = new RetrieveCountriesSpec(validated);
-    const records = await Prisma.country.findMany(spec.toQuery());
+    const query = new RetrieveCountriesSpec(validated).toQuery();
+    const records = await this.prisma.country.findMany(query);
 
     return records.map(record => ({
       id: record.id,
