7b1352e4d3e6ff42040bf6e5f5410fe3f894347f
roney ruiz <roneyruizrojas@gmail.com>
2026-01-30 22:42:13 -0600

    chore: rename retrieve countries spec

---
+++ prisma/source/Application/Handlers/Country/Retrieve/RetrieveSpec.ts
@@ -0,0 +1,15 @@
+import type { CountryFindManyArgs } from "@Prisma/models";
+import type { RetrieveCountriesQuery } from "./RetrieveQuery";
+import { Specification } from "@Database/Specs/Specification";
+
+export class RetrieveCountriesSpec extends Specification {
+  constructor(private query: RetrieveCountriesQuery) {super()};
+
+  public override toQuery() {
+    return {
+      take: this.query.limit,
+      skip: this.query.offset,
+      include: { players: true },
+    } as const satisfies CountryFindManyArgs;
+  };
+};
