2bc182cb4b29da1871b4a9c92e11c99e9137c9e1
roney ruiz <roneyruizrojas@gmail.com>
2026-02-01 20:48:07 -0600

    feat: create retrieve sponsors specification

---
+++ prisma/source/Application/Handlers/Sponsor/Retrieve/RetrieveSpec.ts
@@ -0,0 +1,18 @@
+import type { SponsorFindManyArgs } from "@Prisma/models";
+import type { RetrieveSponsorsQuery } from "./RetrieveQuery";
+import { Specification } from "@Database/Specs/Specification";
+
+export class RetrieveSponsorsSpec extends Specification {
+  constructor(private query: RetrieveSponsorsQuery) {super()};
+
+  public override toQuery() {
+    return {
+      take: this.query.limit,
+      skip: this.query.offset,
+      select: {
+        id: true, name: true,
+        logoUrl: true, websiteUrl: true
+      },
+    } as const satisfies SponsorFindManyArgs;
+  };
+};
