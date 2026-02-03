d9bbbce559bae4e964e3ec3d68b6ecb3f95616c3
roney ruiz <roneyruizrojas@gmail.com>
2026-02-02 21:16:38 -0600

    chore: update entrypoint imports

--- prisma/source/Entrypoint.ts
+++ prisma/source/Entrypoint.ts
@@ -1,6 +1,3 @@
 import { Prisma } from "@Database/Connector";
-import { RetrieveCountriesHandler } from "@Handlers/Country/Exports";
-
-const handler = new RetrieveCountriesHandler(Prisma);
-const records = await handler.handle({});
-console.log(records[0]?.code);
+import { RegisterPlayerHandler } from "@Handlers/Player/Exports";
+import { RetrieveSponsorsHandler } from "@Handlers/Sponsor/Exports";
